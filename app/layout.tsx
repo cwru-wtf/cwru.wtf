f3432987e93cca47c3ccf644cb50bbe9ed1d48fc
ignasxv <ignasxv@gmail.com>
2025-09-02 18:09:01 -0400

    feat: opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,26 @@ export const metadata: Metadata = {
   description: "A collective of CWRU students building the future (or just building cool stuff).",
   icons: {
     icon: '/favicon.svg',
-  }
+  },
+  openGraph: {
+    title: "CWRU.WTF - We Tinker Fearlessly",
+    description: "A collective of CWRU students building the future (or just building cool stuff).",
+    images: [
+      {
+        url: '/cwru-wtf.png',
+        width: 1200,
+        height: 630,
+        alt: 'CWRU.WTF - We Tinker Fearlessly',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "CWRU.WTF - We Tinker Fearlessly",
+    description: "A collective of CWRU students building the future (or just building cool stuff).",
+    images: ['/cwru-wtf.png'],
+  },
 }
 
 export default function RootLayout({
